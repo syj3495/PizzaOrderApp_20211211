286e91683ae07b1b3c5caf0434bb1e4e0ee6a2f6
yeonjin seong <syj3405@naver.com>
2021-12-11 15:15:51 +0900

    프레그먼트 - 결과 가지러 이동,닉네임 액티비티 - 완료 누르면 경과설정 / 복귀

--- app/src/main/java/com/neppplus/pizzaorderapp_20211211/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/EditNicknameActivity.kt
@@ -1,7 +1,9 @@
 package com.neppplus.pizzaorderapp_20211211
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,19 @@ class EditNicknameActivity : AppCompatActivity() {
 
     fun setupEvents(){
 
+        btnOk.setOnClickListener {
+
+            val inputNickname = edtNickname.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick",inputNickname)
+
+            setResult( RESULT_OK, resultIntent )
+            finish()
+
+
+
+        }
 
     }
 
--- app/src/main/java/com/neppplus/pizzaorderapp_20211211/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/fragments/MyProfileFragment.kt
@@ -1,13 +1,18 @@
 package com.neppplus.pizzaorderapp_20211211.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.neppplus.pizzaorderapp_20211211.EditNicknameActivity
 import com.neppplus.pizzaorderapp_20211211.R
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 
 class MyProfileFragment : Fragment() {
+
+    val REQ_FOR_NICKNAME = 1000
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +25,12 @@ class MyProfileFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        btnNicknameEdit.setOnClickListener {
+
+            val myIntent = Intent(requireContext(), EditNicknameActivity::class.java)
+            startActivityForResult(myIntent, REQ_FOR_NICKNAME)
+
+        }
 
 
     }
