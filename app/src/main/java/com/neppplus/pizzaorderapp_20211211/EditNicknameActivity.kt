b9dbde83160e5a33c158144f8fda656185b92275
yeonjin seong <syj3405@naver.com>
2021-12-11 15:09:16 +0900

    닉네임 변경화면 추가

---
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/EditNicknameActivity.kt
@@ -0,0 +1,24 @@
+package com.neppplus.pizzaorderapp_20211211
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+        setupEvents()
+        setValues()
+    }
+
+
+    fun setupEvents(){
+
+
+    }
+
+    fun setValues(){
+
+
+    }
+}
