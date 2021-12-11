1e197c1440a445a0cf8f3756c46137d27c69cfcf
yeonjin seong <syj3405@naver.com>
2021-12-11 14:43:29 +0900

    가게 상세보기 UI작업

---
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/ViewStoreDetalActivity.kt
@@ -0,0 +1,23 @@
+package com.neppplus.pizzaorderapp_20211211
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreDetalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detal)
+
+    }
+
+
+}
+setupEvents(){
+
+}
+
+setValues(){
+
+
+
+}
