2c0199160c0c7cc700f45d61854768163281855e
yeonjin seong <syj3405@naver.com>
2021-12-11 12:22:50 +0900

    뷰페이저 어댑터 기초 작업중  - 상속 / 생성자 해결  - 필수구현 함수는 작업필요

---
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,17 @@
+package com.neppplus.pizzaorderapp_20211211.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class MainViewPagerAdapter( fm:FragmentManager ) : FragmentStatePagerAdapter(fm) {
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+
+    }
+
+}
